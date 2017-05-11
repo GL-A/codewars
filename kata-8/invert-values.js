2f982ad8d263f860f62787732601bfa74cd62dbb
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-05-10 23:28:39 -0700

    lvl8

---
+++ kata-8/invert-values.js
@@ -0,0 +1,18 @@
+// Given a set of numbers, return the additive inverse of
+// each. Each positive becomes negatives, and the negatives
+// become positives.
+
+function invert(array) {
+   return array.map((num)=>{
+     if(num > 0){
+       return num * -1;
+     }
+     if(num < 0 ){
+       return num * -1;
+     } else {
+       return -num;
+     }
+     return num + num;
+   });
+}
+invert([1,2,3,4,5]);// [-1,-2,-3,-4,-5])
