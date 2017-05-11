7dc2f783c18b237c3ea6327dd5ddae25e9a4fa5f
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-05-10 23:51:12 -0700

    more lvl 8s

--- kata-8/invert-values.js
+++ kata-8/invert-values.js
@@ -2,17 +2,20 @@
 // each. Each positive becomes negatives, and the negatives
 // become positives.
 
+// function invert(array) {
+//    return array.map((num)=>{
+//      if(num > 0){
+//        return num * -1;
+//      }
+//      if(num < 0 ){
+//        return num * -1;
+//      } else {
+//        return -num;
+//      }
+//      return num + num;
+//    });
+// }
 function invert(array) {
-   return array.map((num)=>{
-     if(num > 0){
-       return num * -1;
-     }
-     if(num < 0 ){
-       return num * -1;
-     } else {
-       return -num;
-     }
-     return num + num;
-   });
+  return array.map(x => x === 0 ? x : -x);
 }
 invert([1,2,3,4,5]);// [-1,-2,-3,-4,-5])
---
+++ kata-8/string_repeat.js
@@ -0,0 +1,14 @@
+// Write a function called repeatStr which repeats the
+// given string string exactly n times.
+
+
+// function repeatStr (n, s) {
+//   var answer = '';
+//    for(var x = 0; x < n; x ++) {
+//      answer += s;
+//    }
+//   return answer;
+// }
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
