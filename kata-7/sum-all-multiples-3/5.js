4d89eabb03145fabaf5cc3282e643da223d0c958
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-05-27 08:59:18 -0700

    fix

--- kata-7/sum-all-multiples-3/5.js
+++ kata-7/sum-all-multiples-3/5.js
@@ -4,9 +4,7 @@ return the sum of all multiples of 3 and 5.
 
 function findSum(n) {
   var sumArr = [];
-  var mult3 = 0;
-  var mult5 = 0;
-
+  
   for ( var x = 0; x <= n; x ++ ) {
 
   if ( x % 3 === 0 || x % 5 === 0 ) {
